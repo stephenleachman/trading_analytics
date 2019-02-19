fde9a4828ee1271dd079936c1a816ef0116ae6b9
Stephen Leachman <36422934+sjleachman@users.noreply.github.com>
2019-02-19 17:16:12 -0500

    update

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+// FOR PHTO GALLERY LIGHTBOX
+$(document).on('click', '[data-toggle="lightbox"]', function(event){
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+$('.slider').slick({
+  infinite: true,
+  slideToShow:1,
+  slideToScroll:1
+});
